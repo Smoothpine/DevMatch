a8decdb5272295de1c686558c15dc2e821bd9a5b
Philip Lofters <philiplofters@gmail.com>
2017-08-20 14:32:53 +0000

    Added image form field for image upload

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -13,7 +13,7 @@ class ProfilesController < ApplicationController
     @profile = @user.build_profile( profile_params )
     if @profile.save
       flash[:success] = "Profile Updated!"
-      redirect_to root_path
+      redirect_to user_path( params[:user_id] )
     else
       render action: :new
     end
@@ -21,6 +21,6 @@ class ProfilesController < ApplicationController
   
   private
     def profile_params
-      params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :description)
+      params.require(:profile).permit(:first_name, :last_name, :avatar, :job_title, :phone_number, :contact_email, :description)
     end
 end
